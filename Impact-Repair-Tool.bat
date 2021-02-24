795fb273e417f9958305b28df13967dafc707a8b
Zezypisa <49347779+Zezypisa@users.noreply.github.com>
2021-02-24 10:21:41 -0500

    Update Impact-Repair-Tool.bat

--- Impact-Repair-Tool.bat
+++ Impact-Repair-Tool.bat
@@ -225,7 +225,7 @@ goto rcbaritone
 ::===tlauncher===
 :tlauncher
 cls
-start "" https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
 echo Buy the game and use impact
 echo dont pirate games next time :3
 pause
